bbac0b8afb58488960a7637cef99cb9117fa41db
Zac <zknoebel@gmail.com>
2016-09-21 14:40:56 -1000

    updated size() updated add(E e) updated add(int index, E e)
    	modified:   src/MyLinkedList.java

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -2,54 +2,93 @@ import java.util.Comparator;
 
 public class MyLinkedList<E> implements List211<E> {
 
-  MyLinkedList(){
-    // TODO
-  }
-  
-  private class DLinkedNode<E>{
-    
-    DLinkedNode(){
-      
-    }
-    
-    
-  }
-
   private DLinkedNode<E> head;
   private DLinkedNode<E> tail;
-  private int size;
+  private int size = 0;
+
+
+  MyLinkedList(){
+    
+    head = new DLinkedNode(null);
+    tail = new DLinkedNode(null);
+    
+    head.prev = tail;
+    head.next = tail;
+    tail.prev = head;
+    tail.next = head;
+  }
+
+  private class DLinkedNode<E>{
+
+
+    private E data = null;
+    private DLinkedNode<E> next = null;
+    private DLinkedNode<E> prev = null;
+
+    DLinkedNode(E data){
+      this.data = data;
+    } 
+  }
+
 
   public void insertionSort(Comparator<? super E> compare){
     
   }
-  
-  
+
+
   public void bubbleSort(Comparator<? super E> compare){
-    
+
   }
-  
-  
+
+
   public void selectionSort(Comparator<? super E> compare){
-    
+
   }
-  
-  
+
+
   @Override
   public boolean add(E e) {
+      
     // TODO Auto-generated method stub
-    return false;
+    DLinkedNode temp = new DLinkedNode(e);
+    
+    temp.prev = tail.prev;
+    temp.next = tail;
+    tail.prev.next = temp;
+    tail.prev = temp;
+    
+    size ++;
+    
+    return true;
   }
 
 
   @Override
-  public void add(int index, E element) {
-    // TODO Auto-generated method stub
-
+  public void add(int index, E e) {
+    
+    DLinkedNode temp = new DLinkedNode(e);
+    temp.next = head.next;
+    
+    for(int i = 0; i < index; i ++){
+      temp.next = temp.next.next;
+      
+    }
+    
+    temp.prev = temp.next.prev;
+    temp.prev.next = temp;
+    temp.next.prev = temp;
   }
 
 
   @Override
   public E get(int index) {
+    DLinkedNode temp = new DLinkedNode(null);
+    temp.next = head.next;
+    
+    for(int i = 0; i < index; i ++){
+      
+    }
+    
     // TODO Auto-generated method stub
     return null;
   }
@@ -71,8 +110,8 @@ public class MyLinkedList<E> implements List211<E> {
 
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0;
+
+    return size;
   }
 
 }
